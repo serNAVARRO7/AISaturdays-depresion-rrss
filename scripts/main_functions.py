63588ea9d0540ab007482bb1e2a911c7cc503edb
Sergio Navaro <sergio@orvium.io>
2021-01-04 17:31:36 +0100

    Git user test

--- scripts/main_functions.py
+++ scripts/main_functions.py
@@ -75,7 +75,6 @@ def calculateRegression(data,label,resultsummary,alpha):
 		print(list(data.columns))
 		print(reg.coef_,reg.intercept_)
 
-	#Analisis de residuos
 	#influence = fitt.get_influence()
 	#standardized_residuals = influence.resid_studentized_internal
 	#studentized_residuals = influence.resid_studentized_external
